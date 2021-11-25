1e67d0ef32fbe658eaf2708ff411b088b4252e6e
Aleksi012 <aleksi.anttila001@gmail.com>
2021-11-26 00:49:05 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+
+function window_calculation() {
+
+    let lasku1 = Number(document.getElementById("lasku1").value);
+    let lasku2 = Number(document.getElementById("lasku2").value);
+
+    if ( lasku1 == 45 ) {
+        alert("lasku 1 oikein")
+    } else alert("lasku 1 väärin")
+
+    if ( lasku2 == 81 ) {
+        alert("lasku 2 oikein")
+    } else alert("lasku 2 väärin")
+
+}
+
+
